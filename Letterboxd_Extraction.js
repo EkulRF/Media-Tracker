90866f0b580549ca8eadca94dfd5e236016b6bb0
EkulRF <87760589+EkulRF@users.noreply.github.com>
2023-12-20 10:29:50 +0000

    poster fix

--- Letterboxd_Extraction.js
+++ Letterboxd_Extraction.js
@@ -1,75 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('letterboxd-data/diary.csv')
-      .then(response => response.text())
-      .then(csv => {
-          const rows = csv.split('\n');
-          const ratings = []; // Array to store ratings for the histogram
+  // Replace 'your-data.csv' with the path to your CSV file
+  fetch('diary.csv')
+    .then(response => response.text())
+    .then(csv => {
+      const rows = csv.split('\n');
 
-          // Assuming the first row is a header, skip it
-          rows.shift();
+      // Assuming the first row is a header, skip it
+      const header = rows.shift();
 
-          const timelineList = document.getElementById('timeline-list');
+      // Sort the rows by date in ascending order
+      rows.sort((a, b) => {
+        const dateA = new Date(a.split(',')[0].trim());
+        const dateB = new Date(b.split(',')[0].trim());
+        return dateA - dateB;
+      });
 
-          rows.forEach(row => {
-              const values = row.split(',');
+      const timelineList = document.getElementById('timeline-list');
 
-              if (values.length >= 7) {
-                  const rating = parseFloat(values[4].trim()); // Parse rating as a float
+      rows.forEach(async row => {
+        const values = row.split(',');
 
-                  // Add rating to the array
-                  if (!isNaN(rating)) {
-                      ratings.push(rating);
-                  }
+        if (values.length >= 7) {
+          const date = values[0].trim();
+          const name = values[1].trim();
+          const year = values[2].trim();
+          const rating = values[4].trim();
+          const tags = values[6].trim();
 
-                  const date = values[0].trim();
-                  const name = values[1].trim();
-                  const year = values[2].trim();
-                  const tags = values[6].trim();
+          // Make a request to TMDB API to get movie details by name and year
+          const tmdbApiKey = 'YOUR_TMDB_API_KEY'; // Replace with your TMDB API key
+          const tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=${encodeURIComponent(name)}&year=${year}`;
 
-                  // Fetch the movie poster using TMDB API
-                  searchMovie(name, date, year, tags);
-              }
-          });
+          try {
+            const tmdbResponse = await fetch(tmdbUrl);
+            const tmdbData = await tmdbResponse.json();
 
-          // Create the histogram
-          createHistogram(ratings);
-      })
-      .catch(error => console.error('Error fetching CSV:', error));
-});
+            if (tmdbData.results.length > 0) {
+              const posterPath = tmdbData.results[0].poster_path;
 
-function searchMovie(movieName, date, year, tags) {
-  const apiKey = '4fad2cdfc5ea0c55513d2d7cbe65f25e'; // Replace with your TMDB API key
-
-  // Make a request to TMDB API
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(movieName)}`)
-      .then(response => response.json())
-      .then(data => {
-          // Check if there are results
-          if (data.results.length > 0) {
-              const posterPath = data.results[0].poster_path;
-              const posterUrl = `https://image.tmdb.org/t/p/w500/${posterPath}`;
-
-              // Create a timeline item with the movie poster
+              // Create a list item with movie details and poster
               const listItem = document.createElement('li');
               listItem.innerHTML = `
-                  <div class="info">
-                      <img src="${posterUrl}" alt="${movieName} Poster">
-                      <time>${date}</time> ${movieName} (${year}) - Rating: ${rating} - Tags: ${tags}
-                  </div>
+                <div>
+                  <time>${date}</time> ${name} (${year}) - Rating: ${rating} - Tags: ${tags}
+                </div>
+                <div class="poster-container">
+                  <img src="https://image.tmdb.org/t/p/w185${posterPath}" alt="${name} Poster">
+                </div>
               `;
 
               timelineList.appendChild(listItem);
-          } else {
-              // Create a timeline item without the movie poster if no results are found
-              const listItem = document.createElement('li');
-              listItem.innerHTML = `
-                  <div class="info">
-                      <time>${date}</time> ${movieName} (${year}) - Rating: ${rating} - Tags: ${tags}
-                  </div>
-              `;
-
-              timelineList.appendChild(listItem);
+            }
+          } catch (error) {
+            console.error('Error fetching TMDB API:', error);
           }
-      })
-      .catch(error => console.error('Error fetching data:', error));
-}
+        }
+      });
+    })
+    .catch(error => console.error('Error fetching CSV:', error));
+});
